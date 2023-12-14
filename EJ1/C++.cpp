863bcbadd95c9f9ec6a93ab4e3cfe22d50ad1c0c
ZoeAcero <zar.amazona@gmail.com>
2023-12-14 12:42:23 +0100

    1

---
+++ EJ1/C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+//estructura estudiante
+struct estudiante{
+    std::string nombre;
+    int edad;
+    float promedio;
+    std::vector<std::string> materias;
+};
+
