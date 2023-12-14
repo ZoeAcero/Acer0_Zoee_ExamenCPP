00721b0a94e514b24216bc14a35990b5f788d6c9
ZoeAcero <zar.amazona@gmail.com>
2023-12-14 12:42:51 +0100

    2

--- EJ1/C++.cpp
+++ EJ1/C++.cpp
@@ -10,3 +10,16 @@ struct estudiante{
     std::vector<std::string> materias;
 };
 
+//funcion para imprimir los datos de un estudiante
+
+void mostrarEstudiante(const estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+
+    //imprimir materias
+    std::cout << "Materias: ";
+    for (const auto& materia : estudiante.materias) {
+        std::cout << materia << std::endl;
+    }
+}
